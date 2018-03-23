b8cc056ce05f7ea737a217177e88dc085442164e
Ali Chegini <ali.c.engineer@gmail.com>
2018-03-23 18:36:00 +0100

    completed interface

--- Pass-Generator/ViewController.swift
+++ Pass-Generator/ViewController.swift
@@ -10,17 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var guestButon: UIButton!
+    @IBOutlet weak var employeeButton: UIButton!
+    @IBOutlet weak var managerButton: UIButton!
+    @IBOutlet weak var vendorButton: UIButton!
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBOutlet weak var dateOfBirth: UITextField!
+    @IBOutlet weak var ssn: UITextField!
+    @IBOutlet weak var project: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var company: UITextField!
+    @IBOutlet weak var streetAddress: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var zipCode: UITextField!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        test()
+        //test()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func guestActivated(_ sender: UIButton) {
+        button1.setTitle("Child", for: .normal)
+        button2.setTitle("Adult", for: .normal)
+        button3.setTitle("Senior", for: .normal)
+        button4.setTitle("VIP", for: .normal)
+    }
+    
+    
+    @IBAction func childActivated(_ sender: UIButton) {
+        if sender.title(for: .normal) == "Child" {
+            ssn.isEnabled = false
+            ssn.backgroundColor = UIColor.lightGray
+            project.isEnabled = false
+            firstName.isEnabled = false
+            lastName.isEnabled = false
+            company.isEnabled = false
+        }
+    }
+    
 
 }
 
