48638d3aa5a4a058a68ced280114ad1513f7e4d6
Ali Chegini <ali.c.engineer@gmail.com>
2018-04-10 05:07:06 +0200

    keyboard and view fixed

--- Pass-Generator/ViewController.swift
+++ Pass-Generator/ViewController.swift
@@ -37,10 +37,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         //test()
+        
         hideButtons()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
     }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height/2
+            }
+        }
+    }
+    
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height/2
+            }
+        }
+    }
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -87,6 +113,7 @@ class ViewController: UIViewController {
         
         if sender.currentTitle == "Food Service" {
             enableAllTextField()
+            
         }
     }
     
@@ -128,7 +155,24 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func generatePass(_ sender: UIButton) {
+        // How do I know which of the buttons are activated from the first row and second row ?
+        
+        /* Assuming I want to initialize FoodServiceEmployee
+         How to relate to the top buttons
+         do {
+         let foodServiceEmployee = try FoodServiceEmployee(firstName: firstName.text, lastName: lastName.text, streetAddress: streetAddress.text, city: city.text, state: state.text, zipCode: zipCode.text, dateOfBirth: nil)
+         } catch {
+         print("Error: \(error)")
+         }
+         */
+    }
+    
+    
+    
+    
     // Helper methods
+    // Method to hide the buttons at the second row
     func hideButtons() {
         button1.isHidden = true
         button2.isHidden = true
@@ -136,6 +180,7 @@ class ViewController: UIViewController {
         button4.isHidden = true
     }
     
+    // Method to show the buttons at the second row
     func showButtons() {
         button1.isHidden = false
         button2.isHidden = false
